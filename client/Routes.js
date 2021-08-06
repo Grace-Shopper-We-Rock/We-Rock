a2464681846e48f0e1c1a693907636bc46cf2895
Julia Crooijmans <juliacrooijmans@gmail.com>
2021-08-06 15:04:03 -0400

    confirmation page

--- client/Routes.js
+++ client/Routes.js
@@ -8,6 +8,7 @@ import Home from './components/Home'
 import { me } from './store'
 import ProductsList from './components/ProductsList'
 import SingleProduct from './components/SingleProduct'
+import ConfirmationPage from './components/ConfirmationPage'
 import Reviews from './components/Reviews'
 
 /**
@@ -35,6 +36,7 @@ class Routes extends Component {
 						<Route path='/products/:productId' component={SingleProduct} />
 						<Route path='/login' component={Login} />
 						<Route path='/signup' component={SignUp} />
+						<Route path='/confirmationpage/:orderId' component={ConfirmationPage} />
 					</Switch>
 				)}
 			</div>
--- server/db/models/ShippingAddress.js
+++ server/db/models/ShippingAddress.js
@@ -2,6 +2,14 @@ const Sequelize = require('sequelize')
 const db = require('../db')
 
 const ShippingAddress = db.define('shippingAddress', {
+	email: {
+		type: Sequelize.STRING,
+		unique: true,
+		allowNull: false,
+		validate: {
+			isEmail: true,
+		},
+	},
 	firstName: {
 		type: Sequelize.STRING,
 		allowNull: false,
