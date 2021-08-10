bf33da18e1f6b09378e7fcc5a39b03bf5ca4efd4
Alex Fox <acfox7@gmail.com>
2021-08-10 11:15:08 -0400

    Edit All Orders Default

--- client/components/AllOrders.js
+++ client/components/AllOrders.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
-export default AllOrders = (props) => {
-    return (
-        <h1>a</h1>
-    )
+export const AllOrders = (props) => {
+	return <h1>a</h1>
 }
+
+export default AllOrders
